574980c260ac197628cb0b56c9e36e0cbe062718
Naoya Iijima <meteor5963@gmail.com>
2025-08-18 00:46:07 +0900

    style&flutter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import react from '@astrojs/react';
-
+import commonCss from './src/styles/common.css';
 
 export default defineConfig({
     integrations: [
         starlight({
             title: '言語別ドキュメント',
             social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/iijima-naoya-45b' }],
+            customCss: [
+                './src/styles/common.css',
+            ],
             sidebar: [
                 {
                     label: 'Elixir',
